2f672aabc8d75129d16d370bbd0624e9c89877d9
chandu078 <chandudyavanapelli03@gmail.com>
2025-12-13 05:36:02 +0000

    corvette: Initial import proprietary files from PJX110_16.0.1.301(CN01)
    Change-Id: I9592748ec142ded295cd67dd0ef98b700dd35374

---
+++ Android.mk
@@ -0,0 +1,35 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),corvette)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,8df940e653abe30a12b5095d734190c437291ad6)
+$(call add-radio-file-sha1-checked,radio/aop.img,84fe56dfc2ed806937312085f95ab43bac3999bb)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,51c37aedae810a856bf78a24aed876ab8ec28d92)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,3e5835af6b2941f7f85ab0ca3c360a4884c0deae)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,5191171a642bc84e76320fece2383045be7febcd)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,a690e1441a0000f3d3bb4d0ea6f4533cb4976b50)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,1fb8a21adf8ffbdc2dfea8813ec49eb680cbb26e)
+$(call add-radio-file-sha1-checked,radio/dsp.img,72075bb397712d2b7753e2a8654cf8e5c2313602)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,9453e07a03d666732455683946fd52fb53021a1e)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,01cb656b4d0203c22d4577069e2f09cc4b1a4fae)
+$(call add-radio-file-sha1-checked,radio/hyp.img,2532a6a5a5171b67ceeb57ad2041b33775169289)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,7c15344146eb77aa201728b5fd531e74d7c41d20)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,92f51a793ceaf898e3264af8eead856a105aec97)
+$(call add-radio-file-sha1-checked,radio/modem.img,6a18b7182c375b167baf5878312f490f477fee35)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,2a0a3bc41fa77874e80515aaee0a151fd7ea2756)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,5451d999a3a5af2471c891d6b6adb4986bb44730)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,585ed8f1f2914850fe604929506f3a19302b2a4a)
+$(call add-radio-file-sha1-checked,radio/shrm.img,a7766cf9a9624c649010459ea7f9c675765b1b14)
+$(call add-radio-file-sha1-checked,radio/splash.img,07e9cda27d7d55806370294443d78ef887e32d2b)
+$(call add-radio-file-sha1-checked,radio/tz.img,afec8fd48448d02a0406372f32361933d538a934)
+$(call add-radio-file-sha1-checked,radio/uefi.img,0b014ddb1606b37cbd65c7312337426c2395e3b8)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,f35030114478c7b1ee44e07bcb30f4d5cde2dcff)
+$(call add-radio-file-sha1-checked,radio/xbl.img,26e542140623c0d6733942b45686ec53384671f0)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,09e881275663414cf35edc189d938dad6ade8775)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,66e25c60b94b40ba6be809f56392ae4c5d0c0ac8)
+
+endif
